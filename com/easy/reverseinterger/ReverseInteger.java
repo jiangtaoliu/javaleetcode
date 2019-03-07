b755be50d22d227d29cf9753597b0ceefe52ffd3
jiangtaoliu <jet.lau@gmail.com>
2019-03-07 23:20:40 +1300

    reverse integer

--- com/easy/reverseinterger/ReverseInteger.java
+++ com/easy/reverseinterger/ReverseInteger.java
@@ -1,4 +1,35 @@
 package com.easy.reverseinterger;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}
+
 public class ReverseInteger {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int x = Integer.parseInt(line);
+
+            int ret = new Solution().reverse(x);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
 }
