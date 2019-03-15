9ad0381908b5f1558f45e469bf531fb5a6521ff5
jiangtaoliu <jet.lau@gmail.com>
2019-03-15 17:26:54 +1300

    LongestSubstringWithoutRepeatingCharacters

---
+++ com/medium/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,48 @@
+package com.medium.LongestSubstringWithoutRepeatingCharacters;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j <= n; j++)
+                if (allUnique(s, i, j)) ans = Math.max(ans, j - i);
+        return ans;
+    }
+
+    public boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch)) return false;
+            set.add(ch);
+        }
+        return true;
+    }
+}
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static String stringToString(String input) {
+        return JsonArray.readFrom("[" + input + "]").get(0).asString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String s = stringToString(line);
+
+            int ret = new Solution().lengthOfLongestSubstring(s);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
